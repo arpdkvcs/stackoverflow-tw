e09b5725aaf0484dc2e918bccac95cfc6acd3a85
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-09-25 17:07:22 +0200

    Add dependencies Create app configuration env file Create template for env file Add *.env to gitignore Make.properties load from env

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -2,19 +2,29 @@ package com.codecool.stackoverflowtw;
 
 import com.codecool.stackoverflowtw.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.QuestionsDaoJdbc;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StackoverflowTwApplication {
+  private static final String ENV_FILE = "config.env";
 
-    public static void main(String[] args) {
-        SpringApplication.run(StackoverflowTwApplication.class, args);
-    }
+  public static void main(String[] args) {
+    /*
+    Dotenv: load environmental variables from config.env
+      --> application.properties
+    */
+    Dotenv dotenv = Dotenv.configure().filename(ENV_FILE).load();
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    SpringApplication.run(StackoverflowTwApplication.class, args);
+  }
 
-    @Bean
-    public QuestionsDAO questionsDAO() {
-        return new QuestionsDaoJdbc();
-    }
+  @Bean
+  public QuestionsDAO questionsDAO() {
+    return new QuestionsDaoJdbc();
+  }
+
+
 }
