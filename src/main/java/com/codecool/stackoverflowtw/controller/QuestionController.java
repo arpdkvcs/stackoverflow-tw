10a760611b7e6276b4737e852e02066d50e88132
9Andras <117731271+9Andras@users.noreply.github.com>
2023-09-26 11:32:32 +0200

    implement basic frontend with basic fetch

--- src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:5000")
 @RestController
 @RequestMapping("questions")
 public class QuestionController {
