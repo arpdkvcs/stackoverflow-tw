4be0532ea679fe9e0172564b9e745ec41c53b0b7
akos0012 <akosbercel19@gmail.com>
2023-09-26 14:15:53 +0200

    refactor sql in Questions sql 2. task, implement sql queries in answer sql

--- src/main/java/com/codecool/stackoverflowtw/dao/sql/Answers.sql
+++ src/main/java/com/codecool/stackoverflowtw/dao/sql/Answers.sql
@@ -1,6 +1,11 @@
 --Feature to add new answer (location: question detail page)
-;
+INSERT INTO asnwer (id, user_id, content, created_at)
+VALUES (?,?,?,?);
 --Feature to delete answer (location: question delete page) (later extend so only the owner can
-;
+DELETE
+FROM asnwer
+WHERE id=?;
 --OPTIONAL: Feature to edit answers (limited to the ones created by the user)
-;
+UPDATE answer
+SET content = ?
+WHERE id = ?;
--- src/main/java/com/codecool/stackoverflowtw/dao/sql/Questions.sql
+++ src/main/java/com/codecool/stackoverflowtw/dao/sql/Questions.sql
@@ -12,17 +12,12 @@ ORDER BY q.title ASC/*DESC*/;
 --ORDER BY answer_count DESC / ASC;
 
 --2. Detail page of a question shows who asked it and lists all answers with date and users
---2.1.
-SELECT q.id, q.title, q.content, q.created_at, u.username
+SELECT q.id, q.title, q.content, q.created_at, ua.username, a.id, a.content, a.created_at, uq.username
 FROM questions q
-         INNER JOIN users u on q.user_id = u.id
+         INNER JOIN answers a ON q.id = a.question_id
+         INNER JOIN users ua ON a.user_id = ua.id
+         INNER JOIN users uq ON q.user_id = uq.id
 WHERE q.id = ?;
---2.2.
-SELECT a.id, a.content, a.created_at, u.username
-FROM answers a
-         INNER JOIN users u ON a.user_id = u.id
-WHERE A.question_id = ?;
--- TODO: MAKE THIS ONE QUERY
 
 --3. Feature to add new question (redirects to question detail page after save)
 INSERT INTO questions (user_id, title, content)
@@ -31,14 +26,13 @@ VALUES (?, ?, ?);
 --4. Feature to delete question (later extend so only the owner can delete it)
 DELETE
 FROM questions
-WHERE id=?
-  AND user_id = ?;
+WHERE id=?;
 
 --5. Feature to mark the accepted answer for the user's own question
 UPDATE questions
 SET accepted_answer_id = ?
-WHERE id = ?
-  AND user_id = ?;
+WHERE id = ?;
+--  AND user_id = ?;
 
 --6. OPTIONAL: Filter by question titles  (location: main page)
 SELECT *
@@ -50,5 +44,5 @@ WHERE title LIKE '%' || ? || '%';
 UPDATE questions
 SET title   = ?,
     content = ?
-WHERE id = ?
-  AND user_id = ?;;
+WHERE id = ?;
+--  AND user_id = ?;
