4a481beea79d2439d8633883e5fa6c83d1cd22a4
9Andras <117731271+9Andras@users.noreply.github.com>
2023-09-27 15:16:41 +0200

    remove unused import statements

--- src/main/java/com/codecool/stackoverflowtw/service/answer/AnswerService.java
+++ src/main/java/com/codecool/stackoverflowtw/service/answer/AnswerService.java
@@ -1,11 +1,9 @@
 package com.codecool.stackoverflowtw.service.answer;
 
-import com.codecool.stackoverflowtw.controller.dto.answer.AnswerResponseDetailsDTO;
 import com.codecool.stackoverflowtw.controller.dto.answer.NewAnswerDTO;
 import com.codecool.stackoverflowtw.controller.dto.answer.UpdateAnswerDTO;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public interface AnswerService {
     void createAnswer(NewAnswerDTO newAnswerDTO) throws SQLException;
