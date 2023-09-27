8f7a91708457d727785ef1c4b2c97fe0c526e7e2
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-09-27 07:48:05 +0200

    create UserService, impl.

---
+++ src/main/java/com/codecool/stackoverflowtw/service/user/UserService.java
@@ -0,0 +1,17 @@
+package com.codecool.stackoverflowtw.service.user;
+
+import com.codecool.stackoverflowtw.controller.dto.user.UpdateUserDTO;
+import com.codecool.stackoverflowtw.controller.dto.user.UserResponseDTO;
+import com.codecool.stackoverflowtw.controller.dto.user.UserResponseDetailsDTO;
+
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Set;
+
+public interface UserService {
+  Set<UserResponseDTO> readAll() throws SQLException;
+  Optional<UserResponseDetailsDTO> readByUsername(String username) throws SQLException;
+  Optional<UserResponseDetailsDTO> readById(long id) throws SQLException;
+  void update(UpdateUserDTO user) throws SQLException;
+  void delete(long id) throws SQLException;
+}
